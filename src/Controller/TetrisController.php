20e14903cec3fe475f6e3ce1bf53433a8d52d786
Aleksa Nikolic <aleksa@Mac.telekom.ip>
2025-10-30 22:53:24 +0100

    Tetris controller can add entries to database

--- src/Controller/TetrisController.php
+++ src/Controller/TetrisController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use App\Attribute\EnsureVisitor;
+use App\Entity\Tetris;
+use App\Entity\Visitor;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/tetris')]
 final class TetrisController extends AbstractController
 {
-    #[Route('/tetris', name: 'app_tetris')]
-    public function index(): JsonResponse
+    #[Route('/add', name: 'add_tetris', methods: ['POST'])]
+    #[EnsureVisitor]
+    public function add(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TetrisController.php',
-        ]);
+        $tetris = new Tetris();
+        $visitor = $entityManager->getRepository(Visitor::class)->find($request->cookies->get('visitor_id'));
+
+        $body = json_decode($request->getContent(), true);
+        $tetris->setPkt($body['pkt']);
+        $tetris->setFkVisitorId($visitor);
+
+        $entityManager->persist($tetris);
+        $entityManager->flush();
+
+        return new JsonResponse(['success' => true]);
     }
 }
