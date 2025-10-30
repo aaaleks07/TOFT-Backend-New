17cc4adf2c3f7c8e760a3cfc0341d5500dda8c45
Aleksa Nikolic <aleksa@Mac.telekom.ip>
2025-10-30 22:49:04 +0100

    Snake controller can add entries to database

--- src/Controller/SnakeController.php
+++ src/Controller/SnakeController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use App\Attribute\EnsureVisitor;
+use App\Entity\Quiz;
+use App\Entity\Snake;
+use App\Entity\Visitor;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class SnakeController extends AbstractController
 {
-    #[Route('/snake', name: 'app_snake')]
-    public function index(): JsonResponse
+    #[Route('/add', name: 'add_snake', methods: ['POST'])]
+    #[EnsureVisitor]
+    public function add(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SnakeController.php',
-        ]);
+        $snake = new Snake();
+        $visitor = $entityManager->getRepository(Visitor::class)->find($request->cookies->get('visitor_id'));
+
+        $body = json_decode($request->getContent(), true);
+        $snake->setPkt($body['pkt']);
+        $snake->setFkVisitorId($visitor);
+
+        $entityManager->persist($snake);
+        $entityManager->flush();
+
+        return new JsonResponse(['success' => true]);
     }
 }
