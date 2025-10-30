955845f2e1d9397ac5618dee3a20a34a00873ae6
Aleksa Nikolic <aleksa@Mac.telekom.ip>
2025-10-30 22:21:30 +0100

    Quiz controller can add entries to database

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Attribute\EnsureVisitor;
+use App\Entity\Quiz;
+use App\Entity\Visitor;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,12 +15,21 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/quiz')]
 final class QuizController extends AbstractController
 {
-    #[Route('/add', name: 'add_quiz', methods: ['GET','POST'])]
-    public function add(): JsonResponse
+    #[Route('/add', name: 'add_quiz', methods: ['POST'])]
+    #[EnsureVisitor]
+    public function add(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SnakeController.php',
-        ]);
+        $quiz = new Quiz();
+        $visitor = $entityManager->getRepository(Visitor::class)->find($request->cookies->get('visitor_id'));
+
+        $body = json_decode($request->getContent(), true);
+        $quiz->setName($body['name']);
+        $quiz->setPkt($body['pkt']);
+        $quiz->setFkVisitorId($visitor);
+
+        $entityManager->persist($quiz);
+        $entityManager->flush();
+
+        return new JsonResponse(['success' => true]);
     }
 }
